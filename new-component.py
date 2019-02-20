64b61b05cb75a31f4befebf1f76f82e31d0aad9e
syncush <danielhermonsync@gmail.com>
2019-02-20 10:30:23 +0200

    feat - supports sass and ts

--- new-component.py
+++ new-component.py
@@ -2,6 +2,7 @@ import sys
 import os 
 
 name_string = "$componentName"
+styles_extention_token = "$styleExtention"
 dir_path = os.path.dirname(os.path.realpath(__file__))
 template_dir = os.path.join(dir_path, "templates")
 components_dir = os.path.join(template_dir, "components")
@@ -16,12 +17,13 @@ if os.path.exists(component_dir):
 
 os.makedirs(component_dir)
 
-def create_file(template_file_path, new_file_name, spaces):
+def create_file(template_file_path, new_file_name, spaces, styleExtention="css"):
     new_file = open(os.path.join(component_dir, new_file_name), "w+")
     template_file = open(template_file_path, "r")
 
     template_string = template_file.read().replace("    ", spaces)
     new_file_string = template_string.replace(name_string, input_name)
+    new_file_string = template_string.replace(styles_extention_token, styleExtention)
     new_file.write(new_file_string)
 
     new_file.close()
@@ -31,10 +33,19 @@ args_set = set(sys.argv)
 
 spaces = "    "
 if "--2spaces" in args_set:
-    spaces = "  "
+    spaces = " " * 2
+
+styles_extention = 'css'
+if '--sass' in args_set:
+    styles_extention = 'scss'
+
+react_file_extention = 'jsx'
+if '--ts' in args_set:
+    react_file_extention = 'tsx'
+
 
 create_file(os.path.join(template_dir, "index.js.template"), "index.js", spaces)
-create_file(os.path.join(template_dir, "style.css.template"), input_name + ".css", spaces)
+create_file(os.path.join(template_dir, "style.css.template"), input_name + '.' + styles_extention, spaces, styles_extention)
 
 component_template_name = "class"
 if "--hooks" in args_set:
@@ -44,4 +55,4 @@ elif "--func" in args_set or "--function" in args_set:
 
 template_file_path = os.path.join(components_dir,
                         component_template_name + ".template")
-create_file(template_file_path, input_name + ".jsx", spaces)
+create_file(template_file_path, input_name + '.' + react_file_extention, spaces, styles_extention)
